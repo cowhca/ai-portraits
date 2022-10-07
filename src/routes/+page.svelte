3c3d9393fd40d9ed05ecc6f6bdd135eed18e22ef
cowhca <connorwhcallahan@gmail.com>
2022-10-07 14:46:15 -0700

    Upload images and save to server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let fileInput;
+	let files;
+	let images = [];
+
+	function getBase64FromFiles(files) {
+		for (let i = 0; i < files.length; i++) {
+			getBase64(files[i]);
+		}
+	}
+
+	function getBase64(image) {
+		const reader = new FileReader();
+		reader.readAsDataURL(image);
+		reader.onload = (e) => {
+			images = [...images, e.target.result];
+			uploadFunction(e.target.result);
+		};
+	}
+
+	async function uploadFunction(imgBase64) {
+		const data = {};
+		const imgData = imgBase64.split(',');
+		data['image'] = imgData[1];
+		console.log(data);
+		await fetch(`/upload`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: JSON.stringify(data)
+		});
+	}
+</script>
+
+<div>
+	{#each images as image}
+		<img src={image} alt="upload" />
+	{/each}
+	<!-- {#if images}
+		<img src={images} alt="img" />
+	{:else}
+		<img src="placeholder.png" alt="placeholder" />
+	{/if} -->
+	<input
+		multiple
+		class="hidden"
+		id="file-to-upload"
+		type="file"
+		accept=".png,.jpg"
+		bind:files
+		bind:this={fileInput}
+		on:change={() => getBase64FromFiles(files)}
+	/>
+	<button on:click={() => fileInput.click()}>Upload</button>
+</div>
---
+++ src/routes/upload/+server.js
@@ -0,0 +1,11 @@
+import { json } from '@sveltejs/kit';
+import { writeFileSync } from 'fs';
+
+export async function POST({ request }) {
+	const req = await request.json();
+	// console.log(req.image);
+	const file = req.image;
+	writeFileSync('static/image.png', file, 'base64');
+
+	return new json(req);
+}
